2e7d0f15e49970cf1078360e05a85d423f3c71ea
Hideaki Muranami <muranamihideaki@gmail.com>
2017-12-23 06:26:51 +0900

    Modified logbook Models and run migration

--- cinemalog/logbook/models.py
+++ cinemalog/logbook/models.py
@@ -1,26 +1,23 @@
+from datetime import date
 from django.db import models
 
-CREATE TABLE cinemalog (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, date TEXT NOT NULL, track_id TEXT, itunes_url TEXT, preview_url TEXT, img_url30 TEXT, img_url60 TEXT, img_url100 TEXT, title TEXT NOT NULL, director TEXT, genre TEXT, release TEXT, runtime TEXT, score INTEGER, review TEXT);
 class Movie(models.Model):
-    title = models.CharField(max_length=50)
+    """映画"""
+    #id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL
+    date = models.DateTimeField("視聴日", default=date.today)
+    track_id = models.CharField("トラックID", max_length=20, blank=True)
+    itunes_url = models.URLField("iTunesStore URL", blank=True)
+    preview_url = models.URLField("プレビューURL", blank=True)
+    img_url30 = models.URLField("Height30画像URL", blank=True)
+    img_url60 = models.URLField("Height60画像URL", blank=True)
+    img_url100 = models.URLField("Height100画像URL", blank=True)
+    title = models.CharField("タイトル", max_length=50)
+    director = models.CharField("監督", max_length=20, blank=True)
+    genre = models.CharField("ジャンル", max_length=10, blank=True)
+    release = models.CharField("リリース", max_length=4, blank=True)
+    runtime = models.CharField("上映時間", max_length=10, blank=True)
+    score = models.IntegerField("スコア", default=3)
+    review = models.TextField("レビュー", blank=True)
 
     def __str__(self):
-        return "<{0}>".format(self.title)
-
-class Book(models.Model):
-    """書籍"""
-    name = models.CharField('書籍名', max_length=255)
-    publisher = models.CharField('出版社', max_length=255, blank=True)
-    page = models.IntegerField('ページ数', blank=True, default=0)
-
-    def __str__(self):
-        return self.name
-
-
-class Impression(models.Model):
-    """感想"""
-    book = models.ForeignKey(Book, verbose_name='書籍', related_name='impressions')
-    comment = models.TextField('コメント', blank=True)
-
-    def __str__(self):
-        return self.comment
+        return self.title
