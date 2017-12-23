af034d72a0cb71a04ee60619f430c3b4cbd0e192
Hideaki Muranami <muranamihideaki@gmail.com>
2017-12-24 04:15:27 +0900

    Modified logbook/views

--- cinemalog/logbook/views.py
+++ cinemalog/logbook/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse
+from .models import Movie
 
 # Create your views here.
-from django.http.response import HttpResponse
-def hello_world(request):
-    return HttpResponse('Hello World!')
+def index(request):
+    latest_movie_list = Movie.objects.order_by('-date')[:5]
+    output = ', '.join([q.title for q in latest_movie_list])
+    return HttpResponse(output)
+
+def detail(request, movie_id):
+    return HttpResponse("You're looking at movie %s." % movie_id)
+
+def results(request, movie_id):
+    response = "You're looking at the results of movie %s."
+    return HttpResponse(response % movie_id)
+
+def vote(request, movie_id):
+    return HttpResponse("You're voting on movie %s." % movie_id)
