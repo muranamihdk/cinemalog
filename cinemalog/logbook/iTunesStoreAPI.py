d6a851d273f7ece2bce2a9675256c157a2ebc46d
Hideaki Muranami <muranamihideaki@gmail.com>
2017-12-31 23:33:01 +0900

    Add function of saving image

---
+++ cinemalog/logbook/iTunesStoreAPI.py
@@ -0,0 +1,57 @@
+import requests
+
+# URL for debugging
+# - https://itunes.apple.com/search?country=jp&media=movie&term=world
+# - https://itunes.apple.com/lookup?country=jp&id=399966400
+
+## iTunes Store Search API Class
+class iTunesStoreAPI():
+    def __init__(self):
+        self.host = 'itunes.apple.com'
+        self.search_path = '/search'    
+        self.lookup_path = '/lookup'
+        
+    def request(self, opt, path):
+        url = "https://{}{}".format(self.host, path)
+        r = requests.get(url, opt)
+        return r.json()
+
+
+    def search(self, media, entity, keyword):
+        # Parameter
+        opt = {}
+        opt['term'] = keyword
+        opt['country'] = 'JP'
+        opt['media'] = media
+        opt['entity'] = entity
+        opt['lang'] = 'ja_jp'
+        return self.request(opt, self.search_path)
+
+    def lookup(self, item_id):
+        opt = {}
+        opt['id'] = ietm_id
+        opt['country'] = 'JP'
+        return self.request(opt, self.lookup_path)
+
+
+def search_sample_movie():
+    import pprint
+    itunes = iTunesStoreAPI()
+    keyword = 'エデンの東'
+    media = 'movie'
+    entity = 'movie'  #movieArtist, movie
+    results = itunes.search(media, entity, keyword)
+    pprint.pprint(results)
+
+def search_sample_music():
+    import pprint
+    itunes = iTunesStoreAPI()
+    keyword = '未来のミュージアム'
+    media = 'music'
+    entity = 'song'  #musicArtist, musicTrack, album, musicVideo, mix, song
+    results = itunes.search(media, entity, keyword)
+    pprint.pprint(results)
+
+if __name__ == '__main__':
+    search_sample_movie()
+    #search_sample_music()
--- cinemalog/logbook/views.py
+++ cinemalog/logbook/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import get_object_or_404, render, redirect
+from django.http import HttpResponseRedirect
 from django.urls import reverse, reverse_lazy
 from django.views import generic
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from .models import Movie
 from .forms import MovieCreateForm, MovieCreateWithITunesDataForm, MovieUpdateForm
+
+import os
+from urllib.parse import urlparse
+import requests
+from itunesstore.iTunesStoreAPI import iTunesStoreAPI
+
 
 
 class IndexView(generic.ListView):
@@ -34,21 +41,63 @@ class MovieCreateWithITunesData(CreateView):
     form_class = MovieCreateWithITunesDataForm
     template_name = 'movies/create_with_itunes_data.html'
 
+    def _create_image_file_name(self, image_url):
+        img_path = urlparse(image_url)[2]
+        file_name = '{}.{}'.format(img_path.split('/')[-3], img_path.split('/')[-1])
+        return file_name
+
+    def _create_image_path(self, image_file_name):
+        local_path = os.path.join(os.path.dirname(__file__), 'static', 'logbook', 'images', image_file_name[:1], image_file_name)
+        return local_path
+
+    def _save_image(self, image_url, image_file_name):
+        local_path = self._create_image_path(image_file_name)
+        #r = requests.get(image_url)
+        r = requests.get(image_url, stream=True)
+        if r.status_code == 200:
+            with open(local_path, 'wb') as f:
+                #f.write(r.content)
+                for chunk in r.iter_content(chunk_size=1024):
+                    if chunk: # filter out keep-alive new chunks
+                        f.write(chunk)
+
     def get_initial(self):
+        itunes = iTunesStoreAPI()
+        results = itunes.lookup(self.kwargs['track_id'])
+        movie = results['results'][0]
+        img_url30 = self._create_image_file_name(movie['artworkUrl30'])
+        img_url60 = self._create_image_file_name(movie['artworkUrl60'])
+        img_url100 = self._create_image_file_name(movie['artworkUrl100'])
+        self._save_image(movie['artworkUrl30'], img_url30)
+        self._save_image(movie['artworkUrl60'], img_url60)
+        self._save_image(movie['artworkUrl100'], img_url100)
+        runtime = movie.get('trackTimeMillis', '')
+        if runtime:
+            if str(runtime).isdigit():
+                minutes = int(runtime)//60000
+                runtime = '{:d}時間{:d}分'.format(minutes//60, minutes%60)
+        else:
+            runtime = ''
         return {
-            'track_id': self.kwargs['track_id'],
-            #'itunes_url': self.kwargs[''],
-            #'preview_url': self.kwargs[''],
-            #'img_url30': self.kwargs[''],
-            #'img_url60': self.kwargs[''],
-            #'img_url100': self.kwargs[''],
-            #'title': self.kwargs['title'],
-            #'director': self.kwargs[''],
-            #'genre': self.kwargs[''],
-            #'release': self.kwargs[''],
-            #'runtime': self.kwargs[''],
+            'track_id': movie['trackId'],
+            'itunes_url': movie['trackViewUrl'],
+            'preview_url': movie.get('previewUrl', ''),
+            'img_url30': img_url30,
+            'img_url60': img_url60,
+            'img_url100': img_url100,
+            'title': movie['trackName'],
+            'director': movie['artistName'],
+            'genre': movie['primaryGenreName'],
+            'release': movie['releaseDate'][:4],
+            'runtime': runtime,
         }
         #fields = ['date', 'track_id', 'itunes_url', 'preview_url', 'img_url30', 'img_url60', 'img_url100', 'title', 'director', 'genre', 'release', 'runtime', 'score', 'review']
+
+        def form_valid(self, form):
+            self.object = form.save()
+            # do something with self.object
+            return HttpResponseRedirect(self.get_success_url())
+
 
 class MovieUpdate(UpdateView):
     model = Movie
