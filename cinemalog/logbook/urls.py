24ce90d9f158201542074483ef80ac7d0758cddf
Hideaki Muranami <muranamihideaki@gmail.com>
2018-01-14 14:37:14 +0900

    Modified URLconf of logbook:addwithdata

--- cinemalog/logbook/urls.py
+++ cinemalog/logbook/urls.py
@@ -11,8 +11,8 @@ urlpatterns = [
     path('<int:pk>/edit/', views.MovieUpdate.as_view(), name='edit'),
     # ex: /movies/5/delete/
     path('<int:movie_id>/delete/', views.delete, name='delete'),
-    # ex: /movies/add/?track_id=1234567
-    path('add/?track_id=<int:track_id>', views.MovieCreateWithITunesData.as_view(), name='addwithdata'),
+    # ex: /movies/add/itunestrack/1234567
+    path('add/itunestrack/<int:track_id>', views.MovieCreateWithITunesData.as_view(), name='addwithdata'),
     # ex: /movies/add/
     path('add/', views.MovieCreate.as_view(), name='add'),
 ]
